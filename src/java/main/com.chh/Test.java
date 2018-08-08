9479ff077e56d5ce69e3d8b5edc87d63c514dd1e
yangdaye <leme91@163.com>
2018-08-08 11:47:15 +0800

    新增 删除

--- src/java/main/com.chh/Test.java
+++ src/java/main/com.chh/Test.java
@@ -3,8 +3,6 @@ package main.com.chh;
 public class Test {
     public static void main(String[] args) {
         System.out.println("@@!@!@");
-        System.out.println("12312312");
-        System.out.println("323423423fsdfwef");
-        System.out.println("24324sdfhsdfdf");
+
     }
 }
