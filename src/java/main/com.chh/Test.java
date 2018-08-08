e356c81835af940054aea4ff51ba586207b7a678
yangdaye <leme91@163.com>
2018-08-08 13:09:40 +0800

    chh8次更改

--- src/java/main/com.chh/Test.java
+++ src/java/main/com.chh/Test.java
@@ -8,12 +8,10 @@ public class Test {
         System.out.println("m更改");
         System.out.println("m2更改");
 
-        System.out.println("323423423fsdfwef");
+        System.out.println("323423423fsdfwefchh2次更改");
         System.out.println("chh2次更改");
         System.out.println("chh3次更改");
         System.out.println("chh4次更改");
         System.out.println("chh5次更改");
-
-
     }
 }
