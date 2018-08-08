6cac1ea7560eb7feca371518e42e0df43025af33
yangdaye <leme91@163.com>
2018-08-08 11:41:16 +0800

    新增 测试 3

--- src/java/main/com.chh/Test.java
+++ src/java/main/com.chh/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("@@!@!@");
         System.out.println("12312312");
+        System.out.println("323423423fsdfwef");
     }
 }
